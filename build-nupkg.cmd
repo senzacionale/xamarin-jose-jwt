b51595d16b87461b6be82d4227ae1b47d823f4da
senzacionale <mitja.bombac@gmail.com>
2015-12-29 16:58:03 +0100

    Removed unused dependencies

--- build-nupkg.cmd
+++ build-nupkg.cmd
@@ -1,13 +1,1 @@
-tools\nuget.exe update -self
-
-if not exist package mkdir package
-if not exist package\content mkdir package\content
-if not exist package\lib mkdir package\lib
-if not exist package\lib\4.0 mkdir package\lib\4.0
-
-copy JWT\bin\Release\jose-jwt.dll package\lib\4.0\
-copy LICENSE package\content
-
-tools\nuget.exe pack Xamarin-JOSE-JWT.nuspec -BasePath package
-
-pause
+nuget pack JWT\JWT.csproj -IncludeReferencedProjects -Prop Configuration=Release
